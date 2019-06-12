d32d1cabf079b70c729efd5f9fcd6c902137d768
liam humphreys <liam.humphreys1989@gmail.com>
2019-06-12 17:45:25 +0100

    add validation to main

--- lib/ex_gcloud_pubsub_puller.ex
+++ lib/ex_gcloud_pubsub_puller.ex
@@ -6,15 +6,42 @@ defmodule ExGcloudPubsubPuller do
 
   @doc """
   Main entrypoint to a pull job.
+
+  Expects to be called with a module that implements the `ExGcloudPubsubPuller.PullController` behaviour.
   """
   @spec main(pull_controller()) :: any()
   def main(pull_controller) when is_atom(pull_controller) do
+    _pull_controller = validate_pull_controller!(pull_controller)
   end
 
   def main(invalid_arg) do
     raise_invalid_pull_controller(invalid_arg)
   end
 
+  @spec validate_pull_controller!(pull_controller()) :: pull_controller()
+  defp validate_pull_controller!(some_module) do
+    case Code.ensure_loaded(some_module) do
+      {:module, handler_module} ->
+        cond do
+          function_exported?(handler_module, :subscription_id, 0) and
+            function_exported?(handler_module, :handle_stagnant, 0) and
+            function_exported?(handler_module, :handle_pull_error, 1) and
+              function_exported?(handler_module, :handle_message, 1) ->
+            handler_module
+
+          true ->
+            raise_invalid_pull_controller(some_module)
+        end
+
+      error ->
+        raise ArgumentError,
+              "Error ensuring pull controller module #{inspect(some_module)} was loaded: #{
+                inspect(error)
+              }"
+    end
+  end
+
+  @spec raise_invalid_pull_controller(any()) :: any()
   defp raise_invalid_pull_controller(arg) do
     raise ArgumentError,
       message:
--- test/ex_gcloud_pubsub_puller_test.exs
+++ test/ex_gcloud_pubsub_puller_test.exs
@@ -7,6 +7,39 @@ defmodule ExGcloudPubsubPullerTest do
       assert_raise ArgumentError, fn ->
         ExGcloudPubsubPuller.main(fn -> nil end)
       end
+
+      assert_raise ArgumentError, fn ->
+        ExGcloudPubsubPuller.main(:some_atom)
+      end
+
+      assert_raise ArgumentError, fn ->
+        defmodule IncompleteModule do
+        end
+
+        ExGcloudPubsubPuller.main(IncompleteModule)
+      end
+
+      assert_raise ArgumentError, fn ->
+        defmodule CostPullController do
+          @behaviour ExGcloudPubsubPuller.PullController
+
+          @impl true
+          def subscription_id(), do: "cost-subscription"
+
+          @impl true
+          def handle_message(%GoogleApi.PubSub.V1.Model.PubsubMessage{data: nil}), do: :error
+
+          def handle_message(%GoogleApi.PubSub.V1.Model.PubsubMessage{data: data}) do
+            IO.inspect(data)
+            :ok
+          end
+
+          @impl true
+          def handle_stagnant(), do: nil
+        end
+
+        ExGcloudPubsubPuller.main(CostPullController)
+      end
     end
   end
 end
